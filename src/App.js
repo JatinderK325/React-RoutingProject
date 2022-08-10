aa0cd7bbf14a5005f58157baab4cc314d566181d
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-10 19:00:28 -0400

    different urls added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 
 function App() {
   return (
     <Switch>
       {/* route for list of quotes */}
-      <Route path='/quotes'></Route>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+
       {/* route for quotes detail. quoteId = dynamic segment, so that we can load same component for different quotes. here we encoded quote identifier(quoteId) in the url so that we can get it inside of the loaded component. */}
-      <Route path='/quotes/:quoteId'></Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+
       {/* route for adding new quote */}
-      <Route path='/new-quote'></Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
     </Switch>
   );
 }
