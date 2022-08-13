1dd13f01ffd44ef1c5610b35e25c8c056d84734e
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-08-13 14:32:28 -0400

    functionalities added for fetching data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,13 +1,33 @@
+import { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Jatinder', text: 'Learning React is fun!' },
-    { id: 'q2', author: 'Jatinder Kaur', text: 'Learning React is great!' },
-];
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const AllQuotes = () => {
+    const { sendRequest, data: loadedQuotes, status, error } = useHttp(getAllQuotes, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === 'pending') {
+        return <div className='centered'>
+            <LoadingSpinner />
+        </div>
+    }
+
+    if (error) {
+        return <p className="centered focused">{error}</p>
+    }
+
+    if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return <NoQuotesFound />
+    }
+
     return (
-        <QuoteList quotes={DUMMY_QUOTES}></QuoteList>
+        <QuoteList quotes={loadedQuotes}></QuoteList>
     );
 };
 
